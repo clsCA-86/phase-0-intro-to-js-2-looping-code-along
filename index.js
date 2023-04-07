d2f9ccf5a623d08f020d3a153489d336a8855f61
Christopher Smith <c.clshosting@gmail.com>
2023-04-06 21:10:03 -0700

    added index.java

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(namesArray, event) {
+    const messagesArray = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      messagesArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return messagesArray;
+  }
+  
+  
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
